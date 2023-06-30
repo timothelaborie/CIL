c7409c7e7329cb0f0df10ef0e476a2c8f2378180
timothelaborie <tlaborie@student.ethz.ch>
2023-06-30 21:51:44 +0200

    map collector ready

--- mask_to_submission.py
+++ mask_to_submission.py
@@ -15,7 +15,7 @@ flags.DEFINE_string(
 flags.DEFINE_string(
     "base_dir", "data/ethz-cil-road-segmentation-2023/test/pred", "The directory with the predicted masks.")
 
-foreground_threshold = 0.25 # percentage of pixels of val 255 required to assign a foreground label to a patch
+foreground_threshold = 0.3 # percentage of pixels of val 255 required to assign a foreground label to a patch
 
 # assign a label to a patch
 def patch_to_label(patch):
