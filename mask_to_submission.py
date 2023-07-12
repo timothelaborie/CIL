7ccaf3d276c8eb60223e15e8b54c76f04a463576
timothelaborie <tlaborie@student.ethz.ch>
2023-07-12 16:50:49 +0200

    oneformer 0.93984

--- mask_to_submission.py
+++ mask_to_submission.py
@@ -13,7 +13,7 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string(
     "submission_filename", "submission.csv", "The output csv for the submission.")
 flags.DEFINE_string(
-    "base_dir", "data/ethz-cil-road-segmentation-2023/test/pred", "The directory with the predicted masks.")
+    "base_dir", "data/ethz-cil-road-segmentation-2023/test/pred_oneformer", "The directory with the predicted masks.")
 
 foreground_threshold = 0.3 # percentage of pixels of val 255 required to assign a foreground label to a patch
 
